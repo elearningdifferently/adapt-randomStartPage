38382744420481f85b41583395ad5eb647ee3766
Ben Sharp <ben.sharp@rolls-royce.com>
2016-08-09 14:17:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+adapt-randomStartPage
+=====================
+
+An extension that, when enabled, randomly sets the start page - skipping the menu.  The navigation back button is hidden provided your theme contains a class for:
+
+.display-none{ display: hidden; }
---
+++ js/adapt-randomStartPage.js
@@ -0,0 +1,37 @@
+define(function(require) {
+
+	var Adapt = require('coreJS/adapt');
+	
+    // Listen for start up
+    Adapt.once('adapt:start', function () {
+
+		// Get number pages - check we have at least one to go to
+        var numRoutes = Adapt.course.getChildren().length;        
+        if (numRoutes > 0) {
+            
+            // Randomly choose a route
+            var newRoute = 0;
+            if (numRoutes > 1) {
+                newRoute = Math.round(Math.random() * (numRoutes - 1));
+            }
+            
+            // Get new route ID
+            var newRouteID = Adapt.contentObjects.models[newRoute].get('_id');
+            
+            // Jump to new ID
+            window.location.hash = "#/id/" + newRouteID;
+        } else {
+            console.log("No pages found");
+        }
+    });
+    
+    // Listen for page being displayed
+    Adapt.on('router:page', function (model) {
+        
+        // Once all rendering has finished, hide the home button
+        _.defer(function () {
+            $('.navigation-back-button').addClass('display-none');
+        });
+    });
+    
+})
